1cde42bf2cc7ca68918bbfecf427e30c5813446f
marga <margarida.mrro@gmail.com>
2021-11-17 08:52:33 +0000

    Finish proj1-ex1

--- proj1/Fib.hs
+++ proj1/Fib.hs
@@ -4,10 +4,10 @@ fibRec 0 = 0
 fibRec 1 = 1
 fibRec n = fibRec (n-1) + fibRec (n-2) 
 
---1.2 kkkkkkk muito mau isto
-fibLista:: [Integer]
-fibLista = [fibRec (x) | x <- [0..]]
+--1.2 
+fibLista:: (Integral a) => a -> a
+fibLista n = [fibRec (x) | x <- [0..]] !! fromIntegral n
 
 --1.3
-fibListaInfinita:: Int -> [Integer]
-fibListaInfinita n = take n ([fibRec (x) | x <- [0..]])
+fibListaInfinita:: (Integral a)  => a -> a
+fibListaInfinita n = [fibRec (x) | x <- [0..]] !! fromIntegral n
