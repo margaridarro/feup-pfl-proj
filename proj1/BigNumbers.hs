fcbfde25858735532d6b1c03d7a3aa3464bb501a
marga <margarida.mrro@gmail.com>
2021-11-23 12:32:17 +0000

    Fix scanner

--- proj1/BigNumbers.hs
+++ proj1/BigNumbers.hs
@@ -1,5 +1,7 @@
 --2.1 .. 2.7
 import Data.Char (digitToInt)
+import Data.Char (intToDigit)
+import Data.List
 
 
 --2.1
@@ -8,10 +10,17 @@ type BigNumber = (Char, [Int])
 
 --2.2
 scanner:: String -> BigNumber
-scanner (x:xs) = (x, map digitToInt xs)
+scanner (x:xs)  | x == '-' || x == '+' = (x, map digitToInt xs)
+                | otherwise = ('+', digitToInt x : map digitToInt xs)
 
+                
+intToDigit 1
 --2.3
---output
+-- output
+
+-- output:: BigNumber -> String
+-- output (x:xs) = x + concat (xs)
+
 
 --2.4
 --somaBN
