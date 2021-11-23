d91ec2b39ec6a2f888b463d2db5cf392da00409f
marga <margarida.mrro@gmail.com>
2021-11-23 12:00:13 +0000

    Add 2.1 and 2.2

--- proj1/BigNumbers.hs
+++ proj1/BigNumbers.hs
@@ -1,1 +1,26 @@
 --2.1 .. 2.7
+import Data.Char (digitToInt)
+
+
+--2.1
+
+type BigNumber = (Char, [Int])
+
+--2.2
+scanner:: String -> BigNumber
+scanner (x:xs) = (x, map digitToInt xs)
+
+--2.3
+--output
+
+--2.4
+--somaBN
+
+--2.5
+--subBN
+
+--2.6
+--mulBN
+
+--2.7
+--divBN:: BigNumber -> BigNumber -> (BigNumber, BigNumber)
